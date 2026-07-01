0679f2c96efb0e945871d83ded095b804688ec30
Razvan Horhat <razvan99h@yahoo.com>
2026-07-01 18:28:40 +0100

    Added repository

---
+++ repository/repository.py
@@ -0,0 +1,13 @@
+class Repository:
+    def __init__(self, entities_list):
+        self.__entities_list = entities_list
+
+    def add(self, entity):
+        pass
+
+    def remove(self, entity):
+        pass
+
+    def get_all(self):
+        pass
+
