32f767afff0ffff19b6f019a4c8b11378b8489e6
Razvan Horhat <razvan99h@yahoo.com>
2026-07-01 19:53:33 +0100

    feat: Added Caretaker logic

--- ui/ui.py
+++ ui/ui.py
@@ -1,18 +1,29 @@
 from domain.animal import Animal
+from domain.caretaker import Caretaker
 from service.animal_service import AnimalService
 from domain.exceptions import EmptyExceptions
+from service.caretaker_service import CaretakerService
+from service.statistics_service import StatisticsService
+
 
 class UI:
     DEFAULT_AGE = 0
 
-    def __init__(self, animal_service: AnimalService):
+    def __init__(self, animal_service: AnimalService, caretaker_service: CaretakerService, statistics_service: StatisticsService):
         self.__animal_service = animal_service
+        self.__caretaker_service = caretaker_service
+        self.__statistics_service = statistics_service
 
     def __print_menu(self):
         print("1. Add animal")
         print("2. Remove animal")
-        print("3. Median age")
+        print("3. Median age of animals")
         print("4. Print all animals")
+        print("5. Add caretaker")
+        print("6. Remove caretaker")
+        print("7. Median age of caretakers")
+        print("8. Print all caretakers")
+        print("9. Animals without caretaker")
         print("0. Exit")
 
     def __validate_age(self, value:str):
@@ -25,6 +36,56 @@ class UI:
             return False
 
         return True
+
+    def __add_caretaker(self):
+        name = input("Enter caretaker name: ")
+        age_str = input("Enter caretaker's age: ")
+        animal_name = input("Enter caretaker's animal: ")
+
+        if not self.__validate_age(age_str):
+            print("Age must be between 0 and 100")
+            return
+
+        age = int(age_str)
+        animal = self.__animal_service.get_animal(animal_name)
+        if animal is None:
+            print(f"Animal named {animal_name} does not exist!")
+        result = self.__caretaker_service.add_caretaker(Caretaker(name, age, animal))
+        if result:
+            print("Successfully added caretaker")
+        else:
+            print("Caretaker already exists")
+
+    def __remove_caretaker(self):
+        name = input("Enter caretaker name: ")
+
+        caretaker = Caretaker(name, UI.DEFAULT_AGE, None)
+        if self.__caretaker_service.delete_caretaker(caretaker):
+            print("Caretaker successfully removed")
+        else:
+            print("Caretaker does not exist")
+
+    def __median_age_caretakers(self):
+        try:
+            result = self.__statistics_service.average_age_caretakers()
+            print(f"Median age of all caretakers is {result}")
+        except EmptyExceptions:
+            print("No caretakers")
+
+    def __print_caretakers(self):
+        caretakers = self.__caretaker_service.get_all_caretakers()
+
+        if len(caretakers) == 0:
+            print("No caretakers")
+            return
+
+        for caretaker in caretakers:
+            print(caretaker)
+
+    def __print_animals_without_caretakers(self):
+        animals_without_caretaker = self.__statistics_service.animals_without_caretaker()
+        for animal in animals_without_caretaker:
+            print(animal)
 
     def __add_animal(self):
         name = input("Enter animal name: ")
@@ -50,9 +111,9 @@ class UI:
         else:
             print("Animal does not exist")
 
-    def __median_age(self):
+    def __median_age_animals(self):
         try:
-            result = self.__animal_service.average_age()
+            result = self.__statistics_service.average_age_animals()
             print(f"Median age is {result}")
         except EmptyExceptions:
             print("No animals")
@@ -70,19 +131,32 @@ class UI:
     def run(self):
         running = True
         while running:
-            self.__print_menu()
-            command = input("Enter your choice: ")
-            print(f"Processing {command}")
-            if command == "0":
-                print("Exiting...")
-                running = False
-            elif command == "1":
-                self.__add_animal()
-            elif command == "2":
-                self.__remove_animal()
-            elif command == "3":
-                self.__median_age()
-            elif command == "4":
-                self.__print_animals()
-            else:
-                print("Invalid command. Please enter a valid command.")
+            try:
+                self.__print_menu()
+                command = input("Enter your choice: ")
+                print(f"Processing {command}")
+                if command == "0":
+                    print("Exiting...")
+                    running = False
+                elif command == "1":
+                    self.__add_animal()
+                elif command == "2":
+                    self.__remove_animal()
+                elif command == "3":
+                    self.__median_age_animals()
+                elif command == "4":
+                    self.__print_animals()
+                elif command == "5":
+                    self.__add_caretaker()
+                elif command == "6":
+                    self.__remove_caretaker()
+                elif command == "7":
+                    self.__median_age_caretakers()
+                elif command == "8":
+                    self.__print_caretakers()
+                elif command == "9":
+                    self.__print_animals_without_caretakers()
+                else:
+                    print("Invalid command. Please enter a valid command.")
+            except Exception as e:
+                print('An unknown error has occurred', e)
